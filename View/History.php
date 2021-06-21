786a5ebe00ee9cda2855be59a5084fe3482d48e2
Mafdy Magdy <mafdymagdy.mm@gmail.com>
2021-06-21 22:09:49 +0200

    Update

--- View/History.php
+++ View/History.php
@@ -2,20 +2,17 @@
 	<tr>
 		<td align="right">
 			<?php
-				if(!ISSET($_POST['sum'])){
+			if (!isset($_POST['sum'])) {
 			?>
 				<form method="POST" action="">
 				</form>
 			<?php
-				}
-				
-				else
-				{
-					$query = mysqli_query($conn, "SELECT * FROM `questions` where Student_id=$idd") or die(mysqli_error());
-					$fetch = mysqli_fetch_array($query);
-				?>
-				<?php
-				}
+			} else {
+				$query = mysqli_query($conn, "SELECT * FROM `questions` where Student_id=$idd") or die(mysqli_error());
+				$fetch = mysqli_fetch_array($query);
+			?>
+			<?php
+			}
 			?>
 		</td>
 	</tr>
