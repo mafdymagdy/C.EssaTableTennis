8a01c16708e5fff4ba496d08615bb52b43e5919b
Mafdy Magdy <mafdymagdy.mm@gmail.com>
2021-06-21 21:33:08 +0200

    Update

--- View/SearchOrders.php
+++ View/SearchOrders.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
- <head>
+<head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
 </head>
 
 <title>Search Orders</title>
@@ -21,218 +21,225 @@
     z-index: 1;
     height: 100%;
     width: 100%;
-}
+  }
 
-.sidepanel {
-  height: 1000px; /* Specify a height */
-  width: 0; /* 0 width - change this with JavaScript */
-  position: fixed; /* Stay in place */
-  z-index: 1; /* Stay on top */
-  top: 0;
-  right: 0;
-  background-color: #FFF; /* white*/
-  overflow-x: hidden; /* Disable horizontal scroll */
-  padding-top: 60px; /* Place content 60px from the top */
-  transition: 0.5s; /* 0.5 second transition effect to slide in the sidepanel */
-}
+  .sidepanel {
+    height: 1000px;
+    /* Specify a height */
+    width: 0;
+    /* 0 width - change this with JavaScript */
+    position: fixed;
+    /* Stay in place */
+    z-index: 1;
+    /* Stay on top */
+    top: 0;
+    right: 0;
+    background-color: #FFF;
+    /* white*/
+    overflow-x: hidden;
+    /* Disable horizontal scroll */
+    padding-top: 60px;
+    /* Place content 60px from the top */
+    transition: 0.5s;
+    /* 0.5 second transition effect to slide in the sidepanel */
+  }
 
-/* The sidepanel links */
-.sidepanel a {
-  padding: 8px 8px 8px 32px;
-  text-decoration: none;
-  font-size: 25px;
-  color: #111;
-  display: block;
-  transition: 0.3s;
-}
+  /* The sidepanel links */
+  .sidepanel a {
+    padding: 8px 8px 8px 32px;
+    text-decoration: none;
+    font-size: 25px;
+    color: #111;
+    display: block;
+    transition: 0.3s;
+  }
 
-/* When you mouse over the navigation links, change their color */
-.sidepanel a:hover {
-  color: #FFA500;
-}
+  /* When you mouse over the navigation links, change their color */
+  .sidepanel a:hover {
+    color: #FFA500;
+  }
 
-/* Position and style the close button (top right corner) */
-.sidepanel .closebtn {
-  position: absolute;
-  top: 0;
-  right: 25px;
-  font-size: 36px;
-  margin-left: 50px;
-}
+  /* Position and style the close button (top right corner) */
+  .sidepanel .closebtn {
+    position: absolute;
+    top: 0;
+    right: 25px;
+    font-size: 36px;
+    margin-left: 50px;
+  }
 
-/* Style the button that is used to open the sidepanel */
-.openbtn {
-  font-size: 20px;
-  cursor: pointer;
-  background-color: #808080;
-  color: white;
-  padding: 10px 15px;
-  border: none;
-}
+  /* Style the button that is used to open the sidepanel */
+  .openbtn {
+    font-size: 20px;
+    cursor: pointer;
+    background-color: #808080;
+    color: white;
+    padding: 10px 15px;
+    border: none;
+  }
 
-.openbtn:hover {
-  background-color: #FFA500;
-}	
+  .openbtn:hover {
+    background-color: #FFA500;
+  }
 
-/* Fixed sidenav, full height */
-.sidenav {
-  height: 100%;
-  width: 200px;
-  position: fixed;
-  z-index: 1;
-  top: 0;
- right: 0;
-  background-color: #111;
-  overflow-x: hidden;
-  padding-top: 20px;
-}
+  /* Fixed sidenav, full height */
+  .sidenav {
+    height: 100%;
+    width: 200px;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    right: 0;
+    background-color: #111;
+    overflow-x: hidden;
+    padding-top: 20px;
+  }
 
-/* Style the sidenav links and the dropdown button */
-.sidenav a, .dropdown-btn {
-  padding: 6px 8px 6px 16px;
-  text-decoration: none;
-  font-size: 20px;
-  color: #111;
-  display: block;
-  border: none;
-  background: none;
-  width:100%;
-  text-align: left;
-  cursor: pointer;
-  outline: none;
-}
+  /* Style the sidenav links and the dropdown button */
+  .sidenav a,
+  .dropdown-btn {
+    padding: 6px 8px 6px 16px;
+    text-decoration: none;
+    font-size: 20px;
+    color: #111;
+    display: block;
+    border: none;
+    background: none;
+    width: 100%;
+    text-align: left;
+    cursor: pointer;
+    outline: none;
+  }
 
-/* On mouse-over */
-.sidenav a:hover, .dropdown-btn:hover {
-  color: #FFA500;
-}
+  /* On mouse-over */
+  .sidenav a:hover,
+  .dropdown-btn:hover {
+    color: #FFA500;
+  }
 
-/* Main content */
-.main {
-  margin-left: 200px; /* Same as the width of the sidenav */
-  font-size: 20px; /* Increased text to enable scrolling */
-  padding: 0px 10px;
-}
+  /* Main content */
+  .main {
+    margin-left: 200px;
+    /* Same as the width of the sidenav */
+    font-size: 20px;
+    /* Increased text to enable scrolling */
+    padding: 0px 10px;
+  }
 
-/* Add an active class to the active dropdown button */
-.active {
-  background-color: orange;
-  color: black;
-}
+  /* Add an active class to the active dropdown button */
+  .active {
+    background-color: orange;
+    color: black;
+  }
 
-/* Dropdown container (hidden by default). Optional: add a lighter background color and some left padding to change the design of the dropdown content */
-.dropdown-container {
-  display: none;
-  background-color: #FFF;
-  padding-left: 8px;
-}
+  /* Dropdown container (hidden by default). Optional: add a lighter background color and some left padding to change the design of the dropdown content */
+  .dropdown-container {
+    display: none;
+    background-color: #FFF;
+    padding-left: 8px;
+  }
 
-/* Optional: Style the caret down icon */
-.fa-caret-down {
-  float: right;
-  padding-right: 8px;
-}
-    
+  /* Optional: Style the caret down icon */
+  .fa-caret-down {
+    float: right;
+    padding-right: 8px;
+  }
 </style>
 
 <!-- Header -->
 <header>
-            <div class="flex container">
-                
-                <nav>
-                    <button id="nav-toggle" class="hamburger-menu">
-                        <span class="strip"></span>
-                        <span class="strip"></span>
-                        <span class="strip"></span>
-                    </button>
-                    
-                    <ul id="nav-menu"> 
-                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<div style = "position: fixed; right: 100px; top:30px;">
-                        
-                        <li><a href="Admin.php"><img src="images/C.EssaTableTennis/Logo1.jpg" alt="Logo" style="width:70px;height:70px;  position:fixed; left:20px; top:7px;"></a></li>
-                        
-                        <li><i class="fa fa-home"></i> <a href="Admin.php" style = "text-decoration: none;">Home</a></li>
-                        
-                        <li><a href="Courses.php"> A/E/D Product </a></li>
+  <div class="flex container">
+    <nav>
+      <button id="nav-toggle" class="hamburger-menu">
+        <span class="strip"></span>
+        <span class="strip"></span>
+        <span class="strip"></span>
+      </button>
 
-                        <li ><a href="RespondQuestions.php"> Message History </a></li>
-                        
-                        <li><a href="AddAdmin.php"> Add Admins </a></li>
-                        <li><a href="AllAdmins.php"> Delete Admins </a></li>
-                        <li><a href="Allprofiles.php"> All Traniee Profiles </a></li>
-                        <li ><a href="logout.php"> Logout </a></li> 
-                        </div>
-                    </ul>
-                    
-                </nav>
-            </div>
-    </header>
+      <ul id="nav-menu">
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<div style="position: fixed; right: 100px; top:30px;">
+
+          <li><a href="Admin.php"><img src="images/C.EssaTableTennis/Logo1.jpg" alt="Logo" style="width:70px;height:70px;  position:fixed; left:20px; top:7px;"></a></li>
+
+          <li><i class="fa fa-home"></i> <a href="Admin.php" style="text-decoration: none;">Home</a></li>
+
+          <li><a href="Courses.php"> A/E/D Product </a></li>
+
+          <li><a href="RespondQuestions.php"> Message History </a></li>
+
+          <li><a href="AddAdmin.php"> Add Admins </a></li>
+          <li><a href="AllAdmins.php"> Delete Admins </a></li>
+          <li><a href="Allprofiles.php"> All Traniee Profiles </a></li>
+          <li><a href="logout.php"> Logout </a></li>
+        </div>
+      </ul>
+
+    </nav>
+  </div>
+</header>
 
 <!-- Body -->
 <body>
-    
-    <div id="header-hero-container">
-  <section id="hero">
+  <div id="header-hero-container">
+    <section id="hero">
       <div class="fade"></div>
       <div class="hero-text">
-          
-          <!-- F el noss -->
-          <h1> Live Search about any order Down </h1>
-       </div>
-      </section>
+
+        <!-- F el noss -->
+        <h1> Live Search about any order Down </h1>
+      </div>
+    </section>
   </div>
-    
-    <div class="container">
-			<br />
-			<br />
-			<br />
-			<h2 align="center" style="color:red;">Search Now about any order </h2><br />
-			<div class="form-group">
-				<div class="input-group">
-					<span class="input-group-addon">Search</span>
-					<input type="text" name="search_text" id="search_text" placeholder="Search by Trainee Name or Course Name or Course Price " class="form-control" />
-				</div>
-			</div>
-			<br />
-			<div id="result"></div>
-		</div>
-		<div style="clear:both"></div>
-		<br />
-		<br />
-		<br />
-		<br />
-    
-<!-- Footer -->
-<?php require_once("Footer.php") ?>   
-    
-<script>
-$(document).ready(function(){
-	load_data();
-	function load_data(query)
-	{
-		$.ajax({
-			url:"fetchOrders.php",
-			method:"post",
-			data:{query:query},
-			success:function(data)
-			{
-				$('#result').html(data);
-			}
-		});
-	}
-	
-	$('#search_text').keyup(function(){
-		var search = $(this).val();
-		if(search != '')
-		{
-			load_data(search);
-		}
-		else
-		{
-			load_data();			
-		}
-	});
-});
-</script>
+
+  <div class="container">
+    <br />
+    <br />
+    <br />
+    <h2 align="center" style="color:red;">Search Now about any order </h2><br />
+    <div class="form-group">
+      <div class="input-group">
+        <span class="input-group-addon">Search</span>
+        <input type="text" name="search_text" id="search_text" placeholder="Search by Trainee Name or Course Name or Course Price " class="form-control" />
+      </div>
+    </div>
+    <br />
+    <div id="result"></div>
+  </div>
+  <div style="clear:both"></div>
+  <br />
+  <br />
+  <br />
+  <br />
+
+  <script>
+    $(document).ready(function() {
+      load_data();
+
+      function load_data(query) {
+        $.ajax({
+          url: "fetchOrders.php",
+          method: "post",
+          data: {
+            query: query
+          },
+          success: function(data) {
+            $('#result').html(data);
+          }
+        });
+      }
+
+      $('#search_text').keyup(function() {
+        var search = $(this).val();
+        if (search != '') {
+          load_data(search);
+        } else {
+          load_data();
+        }
+      });
+    });
+  </script>
+
+  <!-- Footer -->
+  <?php require_once("Footer.php") ?>
 </body>
-</html> 
+</html>
