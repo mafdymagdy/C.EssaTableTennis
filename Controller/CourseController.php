62becfea723afc68888ffff1dd23339f2d0a1eda
Mafdy Magdy <mafdymagdy.mm@gmail.com>
2021-06-22 14:41:18 +0200

    Update

--- Controller/CourseController.php
+++ Controller/CourseController.php
@@ -1,53 +1,49 @@
 <?php
-  require_once(__ROOT__ . "Controller/Controller.php");
+require_once(__ROOT__ . "Controller/Controller.php");
 
 class CourseController extends Controller
 {
-    public function insert() 
-    {
-        $course_faculty_id = $_REQUEST['course_faculty_id'];
-        $course_name = $_REQUEST['course_name'];
-        $course_price = $_REQUEST['course_price'];
-        $img = $_REQUEST['img'];
-            
-              $this->model->insertCourse($course_faculty_id,$course_name,$course_price,$img);
+  public function insert()
+  {
+    $course_faculty_id = $_REQUEST['course_faculty_id'];
+    $course_name = $_REQUEST['course_name'];
+    $course_price = $_REQUEST['course_price'];
+    $img = $_REQUEST['img'];
 
-    }
-    
-        public function insertcart() 
-    {
-	$user_id = $_REQUEST['user_id'];
-	$user_name = $_REQUEST['user_name'];
-	$course_id = $_REQUEST['course_id'];
-	$course_name = $_REQUEST['course_name'];
-	$course_pricee = $_REQUEST['course_pricee'];
-      
-              $this->model->insertCart($user_id,$user_name,$course_id,$course_name,$course_pricee);
+    $this->model->insertCourse($course_faculty_id, $course_name, $course_price, $img);
+  }
 
-    }
-    
-    public function edit()
-    {
-        $course_faculty_id = $_REQUEST['course_faculty_id'];
-        $course_name = $_REQUEST['course_name'];
-        $course_price = $_REQUEST['course_price'];
-        $img = $_REQUEST['img'];
-        $id =$_REQUEST['id'];
-            
-              $this->model->editCourse($course_faculty_id,$course_name,$course_price,$img,$id);
-      
-    }
+  public function insertcart()
+  {
+    $user_id = $_REQUEST['user_id'];
+    $user_name = $_REQUEST['user_name'];
+    $course_id = $_REQUEST['course_id'];
+    $course_name = $_REQUEST['course_name'];
+    $course_pricee = $_REQUEST['course_pricee'];
 
-    public function delete()
-    {
-      $id = $_REQUEST['id'];
-      $this->model->deleteCourse($id); 
-    }
+    $this->model->insertCart($user_id, $user_name, $course_id, $course_name, $course_pricee);
+  }
 
-    public function deletecart()
-    {
-      $id = $_REQUEST['id'];
-      $this->model->deleteCart($id); 
-    }
+  public function edit()
+  {
+    $course_faculty_id = $_REQUEST['course_faculty_id'];
+    $course_name = $_REQUEST['course_name'];
+    $course_price = $_REQUEST['course_price'];
+    $img = $_REQUEST['img'];
+    $id = $_REQUEST['id'];
+
+    $this->model->editCourse($course_faculty_id, $course_name, $course_price, $img, $id);
+  }
+
+  public function delete()
+  {
+    $id = $_REQUEST['id'];
+    $this->model->deleteCourse($id);
+  }
+
+  public function deletecart()
+  {
+    $id = $_REQUEST['id'];
+    $this->model->deleteCart($id);
+  }
 }
-?>
--- Controller/QuestionController.php
+++ Controller/QuestionController.php
@@ -1,34 +1,31 @@
 <?php
-
-  require_once(__ROOT__ . "controller/Controller.php");
+require_once(__ROOT__ . "controller/Controller.php");
 
 class QuestionController extends Controller
 {
-    public function insert() 
-    {
-    
-	$Student_id = $_REQUEST['student_id'];
-	$question = $_REQUEST['question'];
-	$answer = $_REQUEST['answer'];
+  public function insert()
+  {
 
-              $this->model->insertQuestion($Student_id,$question,$answer);
-        
-    }
-    
-        public function edit()
-    {
-        $Student_id = $_REQUEST['student_id'];
-        $question = $_REQUEST['question'];
-        $answer = $_REQUEST['answer'];
-        $id =$_REQUEST['id'];
-            
-              $this->model->editAnswer($Student_id,$question,$answer, $id);   
-    }
-	
-	 public function delete()
-    {
-      $id = $_REQUEST['id'];
-      $this->model->deleteQuestion($id); 
-    }
+    $Student_id = $_REQUEST['student_id'];
+    $question = $_REQUEST['question'];
+    $answer = $_REQUEST['answer'];
+
+    $this->model->insertQuestion($Student_id, $question, $answer);
   }
-?>
+
+  public function edit()
+  {
+    $Student_id = $_REQUEST['student_id'];
+    $question = $_REQUEST['question'];
+    $answer = $_REQUEST['answer'];
+    $id = $_REQUEST['id'];
+
+    $this->model->editAnswer($Student_id, $question, $answer, $id);
+  }
+
+  public function delete()
+  {
+    $id = $_REQUEST['id'];
+    $this->model->deleteQuestion($id);
+  }
+}
